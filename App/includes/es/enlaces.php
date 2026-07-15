b9f2d6c017b30cc56b2729e5a6003b09118c559d
francxxx <francocedano35@gmail.com>
2026-07-15 14:01:02 +0200

    maquetacion

--- App/includes/es/enlaces.php
+++ App/includes/es/enlaces.php
@@ -13,7 +13,7 @@
 
   <li class="enlaces01__item">
     <a class="enlaces01__link" href="/es/quien-soy">
-      <img class="enlaces01__icon" src="<?= asset('assets/img/icons/users.svg') ?>" alt="">
+      <img class="enlaces01__icon" src="<?= asset('assets/img/icons/somos.png') ?>" alt="">
       <span>Quien soy</span>
     </a>
   </li>
@@ -22,11 +22,11 @@
   <li class="enlaces01__item enlaces01__item--hasSubmenu" data-nav01-submenu-item>
     <div class="enlaces01__submenuHeader">
       <a class="enlaces01__link enlaces01__link--parent" href="/es/servicios">
-        <img class="enlaces01__icon" src="<?= asset('assets/img/icons/boxes.svg') ?>" alt="">
+        <img class="enlaces01__icon" src="<?= asset('assets/img/icons/mano.png') ?>" alt="">
         <span>Mis servicios</span>
       </a>
       <button class="enlaces01__submenuToggle" type="button" aria-controls="<?= e($idSubmenu) ?>" aria-expanded="false" aria-label="Abrir submenú Servicios" data-nav01-submenu-toggle data-nav01-label-open="Abrir submenú" data-nav01-label-close="Cerrar submenú" data-nav01-submenu-label="Servicios">
-        <img class="enlaces01__chevron" src="<?= asset('assets/img/icons/chevron.svg') ?>" alt="">
+        <img class="enlaces01__chevron" src="<?= asset('assets/img/icons/flecha.png') ?>" alt="">
       </button>
     </div>
 
@@ -34,28 +34,28 @@
 
       <li class="enlaces01__item">
         <a class="enlaces01__link enlaces01__link--child" href="/es/servicios/diseño">
-          <img class="enlaces01__icon" src="<?= asset('assets/img/icons/tag.svg') ?>" alt="">
+          <img class="enlaces01__icon" src="<?= asset('assets/img/icons/diseño.png') ?>" alt="">
           <span>Diseño Gráfico</span>
         </a>
       </li>
 
       <li class="enlaces01__item">
         <a class="enlaces01__link enlaces01__link--child" href="/es/servicios/branding">
-          <img class="enlaces01__icon" src="<?= asset('assets/img/icons/spark.svg') ?>" alt="">
+          <img class="enlaces01__icon" src="<?= asset('assets/img/icons/bran.png') ?>" alt="">
           <span>Branding e Identidad Visual</span>
         </a>
       </li>
 
       <li class="enlaces01__item">
         <a class="enlaces01__link enlaces01__link--child" href="/es/servicios/web">
-          <img class="enlaces01__icon" src="<?= asset('assets/img/icons/spark.svg') ?>" alt="">
+          <img class="enlaces01__icon" src="<?= asset('assets/img/icons/web.png') ?>" alt="">
           <span>Diseño Web</span>
         </a>
       </li>
 
       <li class="enlaces01__item">
         <a class="enlaces01__link enlaces01__link--child" href="/es/servicios/textil">
-          <img class="enlaces01__icon" src="<?= asset('assets/img/icons/spark.svg') ?>" alt="">
+          <img class="enlaces01__icon" src="<?= asset('assets/img/icons/textil.png') ?>" alt="">
           <span>Diseño Textil</span>
         </a>
       </li>
@@ -65,7 +65,7 @@
 
   <li class="enlaces01__item">
     <a class="enlaces01__link" href="/es/contacto">
-      <img class="enlaces01__icon" src="<?= asset('assets/img/icons/mail.svg') ?>" alt="">
+      <img class="enlaces01__icon" src="<?= asset('assets/img/icons/contacto.png') ?>" alt="">
       <span>Contacto</span>
     </a>
   </li>

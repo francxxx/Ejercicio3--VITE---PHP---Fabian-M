a98b635d6cbd12d0af66e6d855d8b50e1cf845cc
francxxx <francocedano35@gmail.com>
2026-07-23 09:36:05 +0200

    con

--- vite.config.js
+++ vite.config.js
@@ -18,10 +18,11 @@ export default defineConfig({
       input: {
         inicio: resolvePath("./src/assets/js/inicio.js"),
         contacto: resolvePath("./src/assets/js/contacto.js"),
+        productos: resolvePath("./src/assets/js/productos.js"),
+        
         equipo: resolvePath("./src/assets/js/equipo.js"),
         legal: resolvePath("./src/assets/js/legal.js"),
-
-       
+        showroom: resolvePath("./src/assets/js/showroom.js"),
       },
       output: {
         entryFileNames: "assets/[name]-[hash].js",
